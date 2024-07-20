923c1e1cdaa00c36297943b6fdb96d7a628150eb
Grand Nabil <akambilawani@gmail.com>
2024-07-21 00:07:48 +0100

    ajout de deux scripts pour l'addition

---
+++ addition sans troisième variable.py
@@ -0,0 +1,4 @@
+a = int(input("Entrez le premier nombre : "))
+b = int(input("Entrez le deuxième nombre: "))
+
+print(a, " + ", b, " = ", a+b)
---
+++ addition.py
@@ -0,0 +1,6 @@
+a = int(input("Entrez le premier nombre : "))
+b = int(input("Entrez le deuxième nombre: "))
+
+c = a + b 
+
+print(a, " + ", b, " = ", c )
