5eb42d24abfc93493c7e316147b101c78e87b852
Grand Nabil <akambilawani@gmail.com>
2024-07-21 10:34:12 +0100

    ajout des autres op

--- addition sans troisième variable.py
+++ addition sans troisième variable.py
@@ -2,3 +2,6 @@ a = int(input("Entrez le premier nombre : "))
 b = int(input("Entrez le deuxième nombre: "))
 
 print(a, " + ", b, " = ", a+b)
+print(a, " - ", b, " = ", a-b)
+print(a, " * ", b, " = ", a*b)
+print(a, " / ", b, " = ", a/b)
